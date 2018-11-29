0da642fa25d5f4ee6105362c88b93bdfb2dfa77d
estellarraganit <34287738+estellarraganit@users.noreply.github.com>
2018-11-28 16:18:17 -0800

    Add files via upload

---
+++ TIVAC/Lab04/sourcecode/main.c
@@ -0,0 +1,77 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/tm4c123gh6pm.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/gpio.h"
+#include "driverlib/timer.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom_map.h"
+#include "inc/hw_gpio.h"
+
+int main(void)
+{
+    uint32_t ui32Period;
+
+    SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
+
+    HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = GPIO_LOCK_KEY;
+    HWREG(GPIO_PORTF_BASE + GPIO_O_CR) = 0x01;
+    HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = 0;
+
+    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE,GPIO_PIN_0);
+    IntEnable(INT_GPIOF);
+    GPIOIntTypeSet(GPIO_PORTF_BASE,GPIO_PIN_0,GPIO_FALLING_EDGE);
+    GPIOIntEnable(GPIO_PORTF_BASE, GPIO_INT_PIN_0);
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+    TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
+
+    ui32Period = ((SysCtlClockGet() / 2) * 3) / 4;
+    TimerLoadSet(TIMER0_BASE, TIMER_A, ui32Period -1);
+
+    IntEnable(INT_TIMER0A);
+    TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+    IntMasterEnable();
+
+    TimerEnable(TIMER0_BASE, TIMER_A);
+
+    while(1)
+    {
+    }
+}
+
+void Timer0IntHandler(void)
+{
+    // Clear the timer interrupt
+    TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+
+    // Read the current state of the GPIO pin and
+    // write back the opposite state
+    if(GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_2))
+    {
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0);
+    }
+    else
+    {
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 4);
+    }
+}
+
+void Timer1IntHandler(void)
+{
+    GPIOIntClear(GPIO_PORTF_BASE, GPIO_INT_PIN_0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER1);
+    TimerConfigure(TIMER1_BASE, TIMER_CFG_PERIODIC);
+    TimerLoadSet(TIMER1_BASE, TIMER_A, SysCtlClockGet()-1);
+    TimerEnable(TIMER1_BASE, TIMER_A);
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0);
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 8);
+
+    TimerDisable(TIMER1_BASE, TIMER_A);
+}
